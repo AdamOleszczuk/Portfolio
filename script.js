7501fe45b2e92517ab8a02029abf4b3315c6a834
Adam Oleszczuk <adam.oleszczuk07@gmail.com>
2023-03-02 14:23:06 +0100

    Dodanie burgera

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ var typingEffect = new Typed(".typing-text",{
     typeSpeed: 100,
     backSpeed: 80,
     backDelay: 2000
+});
+
+const mobileNav = document.querySelector(".top-bar-items");
+const burgerBtn = document.querySelector(".burger")
+
+const topItems = [
+document.querySelector("a.top_bar_item"),
+document.querySelector("a.top_bar_item.i1"),
+document.querySelector("a.top_bar_item.i2"),
+document.querySelector("a.top_bar_item.i3"),
+document.querySelector("a.top_bar_item.i4"),
+document.querySelector("a.top_bar_item.i5")
+];
+burgerBtn.addEventListener("click", function(){
+    mobileNav.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
 })
+
+topItems.forEach((item) => {
+    item.addEventListener("click", function() {
+      mobileNav.classList.remove('active');
+      burgerBtn.classList.remove('active');
+    });
+  });
+  
